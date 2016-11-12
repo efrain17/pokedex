ecf72450e74101dae2198c7d823a2d6d38a3c4c2
Efrain-Ubuntu <carlosefrain17@hotmail.com>
2016-11-12 13:23:54 -0500

    implementation of pokemon search

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -4,6 +4,7 @@
     .controller('PokedexController', ['$rootScope','$scope', '$routeParams', 'pokemonService', function ($rootScope,$scope, $routeParams, pokemonService) {
       var type = $routeParams.type;
       $rootScope.title="Pokédex";
+      var pokemonsOld=[];
 
       if (type) {
         $scope.type = type;
@@ -11,19 +12,27 @@
         pokemonService.byType(type).then(function (data) {
           $scope.pokemons = data
           $scope.groupped = partition(data, 4);
+          pokemonsOld=$scope.pokemons;
         });
       } else {
         pokemonService.all().then(function (data) {
           $scope.pokemons = data;
           $scope.groupped = partition(data, 4);
+          pokemonsOld=$scope.pokemons;
         });
       }
-
 
       function partition(data, n) {
         return _.chain(data).groupBy(function (element, index) {
           return Math.floor(index / n);
         }).toArray().value();
+      }
+
+      $scope.searchPokemon= function(){
+        var searchData=pokemonsOld.filter(function(data){
+          return data.name.toLowerCase().indexOf($scope.textSearch.toLowerCase())!== -1;});
+        $scope.pokemons = searchData;
+        $scope.groupped = partition(searchData, 4);
       }
 
     }])
